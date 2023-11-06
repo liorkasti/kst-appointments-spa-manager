66058a1e1fb5b3f57fb6f2b8f787be6430f40c44
liorkasti <liorkasti@gmail.com>
2023-11-06 14:37:25 +0200

    add mutations to update data server

--- client/src/components/user/hooks/useUser.ts
+++ client/src/components/user/hooks/useUser.ts
@@ -10,12 +10,16 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-async function getUser(user: User | null): Promise<User | null> {
+async function getUser(
+  user: User | null,
+  signal: AbortSignal,
+): Promise<User | null> {
   if (!user) return null;
   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
     `/user/${user.id}`,
     {
       headers: getJWTHeader(user),
+      signal,
     },
   );
   return data.user;
@@ -30,16 +34,20 @@ interface UseUser {
 export function useUser(): UseUser {
   // useQuery call to update user data from server
   const queryClient = useQueryClient();
-  const { data: user } = useQuery(queryKeys.user, () => getUser(user), {
-    initialData: getStoredUser,
-    onSuccess: (received: User | null) => {
-      if (!received) {
-        clearStoredUser();
-      } else {
-        setStoredUser(received);
-      }
+  const { data: user } = useQuery(
+    queryKeys.user,
+    ({ signal }) => getUser(user, signal),
+    {
+      initialData: getStoredUser,
+      onSuccess: (received: User | null) => {
+        if (!received) {
+          clearStoredUser();
+        } else {
+          setStoredUser(received);
+        }
+      },
     },
-  });
+  );
   // const user = null;
 
   // meant to be called from useAuth
@@ -52,7 +60,7 @@ export function useUser(): UseUser {
   function clearUser() {
     // reset user to null in query cache
     queryClient.setQueriesData(queryKeys.user, null);
-    queryClient.refetchQueries('user-appointments');
+    queryClient.refetchQueries([queryKeys.appointments, queryKeys.user]);
   }
 
   return { user, updateUser, clearUser };
